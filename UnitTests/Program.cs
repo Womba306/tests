013c72f0c6ea956b9d0c837cb935bc9c27b5f431
Womba <dimonm307@gmail.com>
2025-04-11 02:06:00 +0300

    Добавлен второй проект в основной репозиторий

---
+++ UnitTests/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnitLite;
+
+namespace ConsoleApp6.Tests
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            // AutoRun запускает все тесты, обнаруженные в этом сборке.
+            return new AutoRun().Execute(args);
+        }
+    }
+}
---
+++ UnitTests/Test.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using ConsoleApp6;
+
+namespace ConsoleApp6.Tests
+{
+    [TestFixture]
+    public class FigureTests
+    {
+        [Test]
+        public void Circle_Area_Correct()
+        {
+            // Arrange
+            var circle = new Circle(5);
+            double expectedArea = Math.PI * Math.Pow(5, 2);
+
+            // Act
+            double actualArea = circle.GetArea();
+
+            // Assert
+            Assert.That(actualArea, Is.EqualTo(expectedArea).Within(1e-6));
+        }
+
+        [Test]
+        public void Triangle_Area_Correct()
+        {
+            // Arrange
+            var triangle = new Triangle(3, 4, 5);
+            double expectedArea = 6;
+
+            // Act
+            double actualArea = triangle.GetArea();
+
+            // Assert
+            Assert.That(actualArea, Is.EqualTo(expectedArea).Within(1e-6));
+        }
+
+        [Test]
+        public void Triangle_IsRightTriangle_ReturnsTrue()
+        {
+            // Arrange
+            var triangle = new Triangle(3, 4, 5);
+
+            // Act
+            bool isRight = triangle.IsRightTriangle();
+
+            // Assert
+            Assert.That(isRight, Is.True);
+        }
+    }
+}
